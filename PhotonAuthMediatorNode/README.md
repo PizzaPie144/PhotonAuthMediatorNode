46102a1ed5620699a7995eb7e2ee8abdd4afdada
ptito <ptitos1998@gmail.com>
2022-09-15 13:10:06 +0300

    Add project files.

---
+++ PhotonAuthMediatorNode/README.md
@@ -0,0 +1,3 @@
+# PhotonAuthMediatorNode
+
+
---
+++ PhotonAuthMediatorNode/server.js
@@ -0,0 +1,76 @@
+'use strict';
+var http = require('http');
+var port = process.env.PORT || 3000;
+
+const express = require("express");
+const /*{ initializeApp }*/ admin = require('firebase-admin/app');
+
+const app = express();
+app.use(express.json());
+
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
+
+const firebaseApp = admin.initializeApp({              //use env var for service account json file path
+    credential: applicationDefault()
+});
+
+const auth = getAuth(firebaseApp);
+
+
+app.get("/auth", (req, res) => {
+
+    const token = req.body.token;
+
+    if (!token) {
+        return res.json(
+            {
+                ResultCode: 3,
+                Message: "invalid request!"
+            })
+    }
+
+    auth.verifyIdToken(token)
+        .then((decodedToken) => {
+            const uid = decodedToken.uid;
+
+            return res.json({
+                ResultCode: 2,
+                Message: "Auth success!"
+            });
+            // ...
+        })
+        .catch((error) => {
+            //could add manual loging here for errors
+
+            return res.json({
+                ResultCode: 2,
+                Message: "Something went wrong! Please try again!"
+            });
+        });
+});
+
+app.listen(port);
+
+
+//http.createServer(function (req, res) {
+//    res.writeHead(200, { 'Content-Type': 'text/plain' });
+//    res.end('Hello World\n');
+//}).listen(port);
+
+//http.get()
+/*
+ * 
+   getAuth()
+  .verifyIdToken(idToken)
+  .then((decodedToken) => {
+    const uid = decodedToken.uid;
+    // ...
+  })
+  .catch((error) => {
+    // Handle error
+  });
+ */
